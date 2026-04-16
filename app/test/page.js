7cf1d6705d2c3e8f93c99bff22de34a480658e23
Moin <moinpass@hotmail.com>
2026-04-16 18:20:38 +0100

    fix player folder structure

--- app/test/page.js
+++
@@ -1,8 +0,0 @@
-export default function TestPage() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Test Page</h1>
-      <p>If you see this on Vercel, deployment works.</p>
-    </div>
-  );
-}
