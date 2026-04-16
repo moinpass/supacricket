e6fc3b9c995ca1aecf10c1f6e757b560c8f19c65
Moin <moin@example.com>
2026-04-16 11:27:50 +0100

    Initial test page

---
+++ app/test/page.js
@@ -0,0 +1,8 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Test Page</h1>
+      <p>If you see this on Vercel, deployment works.</p>
+    </div>
+  );
+}
